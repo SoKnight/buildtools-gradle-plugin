48dd07d8a3339387a3b9d8f7d69283485d8b82ec
SoKnight <mcliyanage2018@gmail.com>
2025-09-08 04:13:38 +0500

    feat: add `BuildSpigotTask`

--- src/main/java/io/github/soknight/gradle/buildtools/service/BuildToolsService.java
+++ src/main/java/io/github/soknight/gradle/buildtools/service/BuildToolsService.java
@@ -1,29 +1,48 @@
 package io.github.soknight.gradle.buildtools.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.gradle.api.file.Directory;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.RegularFile;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.services.BuildService;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-@Slf4j
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public abstract class BuildToolsService implements BuildService<BuildToolsServiceParameters> {
 
     private static final @NotNull String BUILD_INFO_CACHE_DIRECTORY_NAME = "build-info-cache";
+    private static final @NotNull String BUILD_RESULT_CACHE_DIRECTORY_NAME = "build-result-cache";
     private static final @NotNull String BUILD_TOOLS_JAR_FILE_NAME = "BuildTools.jar";
 
-    public BuildToolsService() {
+    private final @NotNull Lock buildToolsLock;
 
+    public BuildToolsService() {
+        this.buildToolsLock = new ReentrantLock();
     }
 
-    public @NotNull Provider<RegularFile> getBuildToolsJarFile() {
-        return getWorkingDirectory().file(BUILD_TOOLS_JAR_FILE_NAME);
+    @ApiStatus.Internal
+    public void lockBuildTools() {
+        this.buildToolsLock.lock();
+    }
+
+    @ApiStatus.Internal
+    public void unlockBuildTools() {
+        this.buildToolsLock.unlock();
     }
 
     public @NotNull Provider<Directory> getBuildInfoCacheDirectory() {
         return getWorkingDirectory().dir(BUILD_INFO_CACHE_DIRECTORY_NAME);
+    }
+
+    public @NotNull Provider<Directory> getBuildResultCacheDirectory() {
+        return getWorkingDirectory().dir(BUILD_RESULT_CACHE_DIRECTORY_NAME);
+    }
+
+    public @NotNull Provider<RegularFile> getBuildToolsJarFile() {
+        return getWorkingDirectory().file(BUILD_TOOLS_JAR_FILE_NAME);
     }
 
     public @NotNull DirectoryProperty getWorkingDirectory() {
---
+++ src/main/java/io/github/soknight/gradle/buildtools/task/BuildSpigotTask.java
@@ -0,0 +1,100 @@
+package io.github.soknight.gradle.buildtools.task;
+
+import io.github.soknight.gradle.buildtools.service.BuildToolsService;
+import org.gradle.api.JavaVersion;
+import org.gradle.api.file.RegularFile;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.logging.LogLevel;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.services.ServiceReference;
+import org.gradle.api.tasks.*;
+import org.gradle.jvm.toolchain.JavaLanguageVersion;
+import org.gradle.jvm.toolchain.JavaLauncher;
+import org.gradle.jvm.toolchain.JavaToolchainService;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.Nullable;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.github.soknight.gradle.buildtools.Constants.DEFAULT_BUILD_VERSION;
+
+@CacheableTask
+public abstract class BuildSpigotTask extends JavaExec {
+
+    public BuildSpigotTask() {
+        getBuildRemappedJars().set(getBuildToolsService().get().getParameters().getBuildRemappedJars());
+        getBuildVersion().set(DEFAULT_BUILD_VERSION);
+        getOutputFile().set(getBuildVersion().map(this::resolveDefaultOutputFile));
+
+        getJavaLauncher().set(getRequiredJavaVersion().flatMap(this::resolveJavaLauncher));
+        getLogging().captureStandardOutput(LogLevel.DEBUG);
+        getLogging().captureStandardError(LogLevel.DEBUG);
+    }
+
+    @Override
+    public void exec() {
+        var buildToolsService = getBuildToolsService().get();
+        args(constructArgs(getOutputFile().get().getAsFile().toPath()));
+        classpath(getProject().files(buildToolsService.getBuildToolsJarFile()));
+        workingDir(buildToolsService.getWorkingDirectory());
+
+        buildToolsService.lockBuildTools();
+        try {
+            super.exec();
+        } finally {
+            buildToolsService.unlockBuildTools();
+        }
+    }
+
+    private @NotNull List<String> constructArgs(@NotNull Path outputPath) {
+        List<String> args = new ArrayList<>();
+        args.add("--nogui");
+
+        args.add("--final-name");
+        args.add(outputPath.getFileName().toString());
+
+        args.add("--output-dir");
+        args.add(outputPath.getParent().toAbsolutePath().toString());
+
+        if (getBuildRemappedJars().getOrElse(Boolean.TRUE))
+            args.add("--remapped");
+
+        args.add("--rev");
+        args.add(getBuildVersion().get());
+        return args;
+    }
+
+    private @Nullable Provider<JavaLauncher> resolveJavaLauncher(@Nullable Integer requiredJavaVersion) {
+        if (requiredJavaVersion == null || requiredJavaVersion <= 0)
+            return null;
+
+        var javaToolchainService = getProject().getExtensions().getByType(JavaToolchainService.class);
+        var javaMajorVersion = JavaVersion.forClassVersion(requiredJavaVersion).getMajorVersion();
+        var javaLanguageVersion = JavaLanguageVersion.of(javaMajorVersion);
+        return javaToolchainService.launcherFor(spec -> spec.getLanguageVersion().set(javaLanguageVersion));
+    }
+
+    private @NotNull RegularFile resolveDefaultOutputFile(@NotNull String buildVersion) {
+        var directory = getBuildToolsService().flatMap(BuildToolsService::getBuildResultCacheDirectory).get();
+        return directory.file("spigot-%s.jar".formatted(buildVersion));
+    }
+
+    @Input @Optional
+    public abstract @NotNull Property<Boolean> getBuildRemappedJars();
+
+    @Input @Optional
+    public abstract @NotNull Property<String> getBuildVersion();
+
+    @Input
+    public abstract @NotNull Property<Integer> getRequiredJavaVersion();
+
+    @OutputFile
+    public abstract @NotNull RegularFileProperty getOutputFile();
+
+    @ServiceReference("buildTools")
+    public abstract @NotNull Property<BuildToolsService> getBuildToolsService();
+
+}
