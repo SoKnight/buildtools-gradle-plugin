e76b4b87b386e63b14e61199551456d7f1afa255
SoKnight <mcliyanage2018@gmail.com>
2025-09-08 01:36:48 +0500

    build: setup project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    alias(libs.plugins.gradle.publish.plugin)
+}
+
+group = "io.github.soknight"
+version = "1.0"
+
+gradlePlugin {
+    vcsUrl = "https://github.com/SoKnight/buildtools-gradle-plugin.git"
+    website = "https://github.com/SoKnight/buildtools-gradle-plugin"
+
+    plugins {
+        create("buildtools-gradle-plugin", fun PluginDeclaration.() {
+            id = "io.github.soknight.buildtools"
+            displayName = "BuildTools Gradle Plugin"
+            description = "Brings the Spigot build with BuildTools into your Gradle project"
+            tags = listOf("minecraft", "spigot", "buildtools")
+            implementationClass = "io.github.soknight.gradle.buildtools.BuildToolsPlugin"
+        })
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.withType<Javadoc> {
+    enabled = false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "buildtools-gradle-plugin"
