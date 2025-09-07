2d06f5eb13ceff71d38863326f1b9f6731892ca9
SoKnight <mcliyanage2018@gmail.com>
2025-09-08 03:41:41 +0500

    feat: change version selector logic to find relevant one

--- src/main/java/io/github/soknight/gradle/buildtools/model/BuildInfoModel.java
+++ src/main/java/io/github/soknight/gradle/buildtools/model/BuildInfoModel.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public record BuildInfoModel(
         @JsonProperty("name") String name,
@@ -19,14 +20,24 @@ public record BuildInfoModel(
 
     public static final int DEFAULT_JAVA_VERSION = 52;  // Java 8
 
-    public @NotNull OptionalInt minimalJavaVersion() {
-        return supportedJavaVersions != null
-                ? supportedJavaVersions.stream().filter(Objects::nonNull).mapToInt(Integer::intValue).min()
-                : OptionalInt.empty();
+    public @NotNull OptionalInt relevantJavaVersion() {
+        if (supportedJavaVersions == null || supportedJavaVersions.isEmpty())
+            return OptionalInt.empty();
+
+        // firstly, iterate over LTS releases: 8, 11, 17, 21 and 25
+        var relevantVersion = IntStream.of(52, 55, 61, 65, 69)
+                .filter(supportedJavaVersions::contains)
+                .max();
+
+        if (relevantVersion.isPresent())
+            return relevantVersion;
+
+        // fallback to minimal supported version
+        return supportedJavaVersions.stream().filter(Objects::nonNull).mapToInt(Integer::intValue).min();
     }
 
-    public int minimalJavaVersionOrDefault() {
-        return minimalJavaVersion().orElse(DEFAULT_JAVA_VERSION);
+    public int relevantJavaVersionOrDefault() {
+        return relevantJavaVersion().orElse(DEFAULT_JAVA_VERSION);
     }
 
 }
