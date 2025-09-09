e8f3902836cae2abfc5691de9503b05e3c0ae048
SoKnight <mcliyanage2018@gmail.com>
2025-09-09 11:54:18 +0500

    refactor: `javaVersions` is a range, lol

--- src/main/java/io/github/soknight/gradle/buildtools/model/BuildInfoModel.java
+++ src/main/java/io/github/soknight/gradle/buildtools/model/BuildInfoModel.java
@@ -3,10 +3,7 @@ package io.github.soknight.gradle.buildtools.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public record BuildInfoModel(
@@ -15,29 +12,49 @@ public record BuildInfoModel(
         @JsonProperty("refs") Map<String, String> gitRefs,
         @JsonProperty("hashes") Map<String, String> hashes,
         @JsonProperty("toolsVersion") Integer buildToolsVersion,
-        @JsonProperty("javaVersions") List<Integer> supportedJavaVersions
+        @JsonProperty("javaVersions") List<Integer> rawJavaVersionRange
 ) {
 
     public static final int DEFAULT_JAVA_VERSION = 52;  // Java 8
 
+    // Java LTS releases: 8, 11, 17, 21 and 25
+    public static final @NotNull Set<Integer> JAVA_LTS_RELEASES = Set.of(52, 55, 61, 65, 69);
+
+    public int minSupportedJavaVersion() {
+        return rawJavaVersionRange != null && rawJavaVersionRange.size() == 2
+                ? rawJavaVersionRange.stream().mapToInt(Integer::intValue).min().orElse(DEFAULT_JAVA_VERSION)
+                : DEFAULT_JAVA_VERSION;
+    }
+
+    public int maxSupportedJavaVersion() {
+        return rawJavaVersionRange != null && rawJavaVersionRange.size() == 2
+                ? rawJavaVersionRange.stream().mapToInt(Integer::intValue).max().orElse(DEFAULT_JAVA_VERSION)
+                : DEFAULT_JAVA_VERSION;
+    }
+
     public @NotNull OptionalInt relevantJavaVersion() {
-        if (supportedJavaVersions == null || supportedJavaVersions.isEmpty())
-            return OptionalInt.empty();
+        int[] supportedJavaVersions = supportedJavaVersions().toArray();
 
         // firstly, iterate over LTS releases: 8, 11, 17, 21 and 25
-        var relevantVersion = IntStream.of(52, 55, 61, 65, 69)
-                .filter(supportedJavaVersions::contains)
+        var relevantVersion = IntStream.of(supportedJavaVersions)
+                .filter(JAVA_LTS_RELEASES::contains)
                 .max();
 
         if (relevantVersion.isPresent())
             return relevantVersion;
 
-        // fallback to minimal supported version
-        return supportedJavaVersions.stream().filter(Objects::nonNull).mapToInt(Integer::intValue).min();
+        // fallback to max supported version
+        return IntStream.of(supportedJavaVersions).max();
     }
 
     public int relevantJavaVersionOrDefault() {
         return relevantJavaVersion().orElse(DEFAULT_JAVA_VERSION);
+    }
+
+    public @NotNull IntStream supportedJavaVersions() {
+        int minVersion = minSupportedJavaVersion();
+        int maxVersion = maxSupportedJavaVersion();
+        return IntStream.rangeClosed(minVersion, maxVersion).distinct();
     }
 
 }
