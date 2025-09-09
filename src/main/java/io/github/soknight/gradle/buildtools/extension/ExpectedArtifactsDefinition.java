d00dd47e301cb3f697924e808425de3b4e3db952
SoKnight <mcliyanage2018@gmail.com>
2025-09-09 19:17:11 +0500

    feat: add expect() and shorthand methods

--- src/main/java/io/github/soknight/gradle/buildtools/extension/ExpectedArtifactsDefinition.java
+++ src/main/java/io/github/soknight/gradle/buildtools/extension/ExpectedArtifactsDefinition.java
@@ -7,6 +7,46 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class ExpectedArtifactsDefinition {
 
+    public void expect(@NotNull ExpectedArtifactType type, boolean isExpected) {
+        getByType().put(type, isExpected);
+    }
+
+    public void bootstrapJar(boolean isExpected) {
+        getBootstrapJar().set(isExpected);
+    }
+
+    public void mappingsMojang(boolean isExpected) {
+        expect(ExpectedArtifactType.MAPPINGS_MOJANG, isExpected);
+    }
+
+    public void mappingsSpigot(boolean isExpected) {
+        expect(ExpectedArtifactType.MAPPINGS_SPIGOT, isExpected);
+    }
+
+    public void mappingsSpigotMembers(boolean isExpected) {
+        expect(ExpectedArtifactType.MAPPINGS_SPIGOT_MEMBERS, isExpected);
+    }
+
+    public void minecraftServerJar(boolean isExpected) {
+        expect(ExpectedArtifactType.MINECRAFT_SERVER_JAR, isExpected);
+    }
+
+    public void spigotApiJar(boolean isExpected) {
+        expect(ExpectedArtifactType.SPIGOT_API_JAR, isExpected);
+    }
+
+    public void spigotJar(boolean isExpected) {
+        expect(ExpectedArtifactType.SPIGOT_JAR, isExpected);
+    }
+
+    public void spigotRemappedMojangJar(boolean isExpected) {
+        expect(ExpectedArtifactType.SPIGOT_REMAPPED_MOJANG_JAR, isExpected);
+    }
+
+    public void spigotRemappedObfJar(boolean isExpected) {
+        expect(ExpectedArtifactType.SPIGOT_REMAPPED_OBF_JAR, isExpected);
+    }
+
     public abstract @NotNull Property<Boolean> getBootstrapJar();
 
     public abstract @NotNull MapProperty<ExpectedArtifactType, Boolean> getByType();
